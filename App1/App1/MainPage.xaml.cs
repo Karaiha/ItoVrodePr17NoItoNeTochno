ebcb5f0bd92ddccdf16804ca88750851830936d5
Анна Кучкина <ami_san@inbox.ru>
2022-05-01 12:21:15 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        string url = "http://numbersapi.com/";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = Convert.ToInt32(eNumber.Text);
+
+                if (num > 0)
+                {
+                    using (WebClient webClient = new WebClient())
+                    {
+                        lFact.Text = webClient.DownloadString(url + eNumber.Text);
+                    }
+                }
+                else
+                {
+                    lFact.Text = "Введите число больше нуля! >:с";
+                }
+            }
+            catch
+            {
+                lFact.Text = "Но... Это же не число... :с";
+            }
+        }
+    }
+}
